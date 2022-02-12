225a6c3154aa190f7bf699705258d2c89537b7f4
tquangdo <trangia61@gmail.com>
2022-02-12 19:04:53 +0900

    first commit

--- terraform-start/vars.tf
+++ terraform-start/vars.tf
@@ -1,4 +1,4 @@
 variable "codestar_connection" {
   type    = string
-  default = "arn:aws:codestar-connections:us-east-1:462123133781:connection/5f3093a2-d850-4ae4-998c-e9c09a95d456"
+  default = "arn:aws:codestar-connections:us-east-1:<AWS_ACCID>:connection/5f3093a2-d850-4ae4-998c-e9c09a95d456"
 }
